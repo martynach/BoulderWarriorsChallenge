db32240bf45219405332ff6cb2a0c9e1d5ff34fe
Martyna <chomicz.martyna@gmail.com>
2018-04-02 22:59:45 +0200

    Creating files with data

--- backend/js/app.js
+++ backend/js/app.js
@@ -8,9 +8,21 @@ const filepath = path.join( __dirname, './../data/players.json');
 const player = new Player(filepath);
 
 
-
+//lista wszystkich zawodników
 app.get('/players', async (req, res) => {
     res.send(await player.getAllPlayers());
+    
+});
+
+//lista wszystkich eventów
+app.get('/meetings', async (req, res) => {
+    
+});
+
+//lista wyników konkretnych zawodów
+app.get('/meetings/:id/results', async (req, res) => {
+    const meetingID = req.params.id;
+
     
 });
 
