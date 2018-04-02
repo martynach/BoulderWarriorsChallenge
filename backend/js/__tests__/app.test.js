2954c252e36c3970ea1de8ed5a6fa0d9ad1811f2
Martyna <chomicz.martyna@gmail.com>
2018-04-03 01:03:33 +0200

    Adding API tests for meetings/:id/results endpoint

--- backend/js/__tests__/app.test.js
+++ backend/js/__tests__/app.test.js
@@ -10,3 +10,12 @@ test('Test /meetings endpoint', async () => {
     return request(app).get('/meetings').expect(200);
 });
 
+test('Test /meetings/:1/results endpoint', async () => {
+    return request(app).get('/meetings/1/results').expect(200);
+});
+
+//todo statusCode
+test('Test /meetings/:2/results endpoint', async () => {
+    return request(app).get('/meetings/2/results').expect(200);
+});
+
