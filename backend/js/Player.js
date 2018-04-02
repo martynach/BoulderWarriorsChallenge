9583a92beeb689a8901359a1153ed5612df4f1c8
Martyna <chomicz.martyna@gmail.com>
2018-04-02 21:37:20 +0200

    sorting players alphabetically

--- backend/js/Player.js
+++ backend/js/Player.js
@@ -7,9 +7,37 @@ class Player {
         this.filepath = filepath;
     }
 
-    async getAllPlayers () {
-        const jsonString = await promisify(fs.readFile, this.filepath, 'utf8');
-        return JSON.parse(jsonString);
+    async getAllPlayers() {
+        if (!this.playersArray) {
+            const jsonString = await promisify(fs.readFile, this.filepath, 'utf8');
+            this.playersArray = JSON.parse(jsonString);
+        }
+        return this.playersArray.sort((player1, player2) => {
+            const lastname1 = player1.lastname.toLowerCase();
+            const lastname2 = player2.lastname.toLowerCase();
+
+            if (lastname1 < lastname2) {
+                return -1;
+            }
+
+            if (lastname1 > lastname2) {
+                return 1;
+            }
+
+            const firstname1 = player1.firstname.toLowerCase();
+            const firstname2 = player2.firstname.toLowerCase();
+
+            if (firstname1 < firstname2) {
+                return -1;
+            }
+
+            if (firstname1 > firstname2) {
+                return 1;
+            }
+
+            return 0;
+        });
+
     }
 
 
