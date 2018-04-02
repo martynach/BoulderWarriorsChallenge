daeb1d7eded8cd56b6cf784b84d0a13b4a1f6629
Martyna <chomicz.martyna@gmail.com>
2018-04-02 20:30:42 +0200

    changing test of getting all players

--- backend/js/_tests_/players.test.js
+++ backend/js/_tests_/players.test.js
@@ -8,6 +8,7 @@ const player = new Player(filepath);
 test('Get all players', async () => {
     expect.assertions(1);
     const actual = JSON.parse(await player.getAllPlayers());
-    const expected = JSON.parse('[{"id":1,"firstname":"Edyta","lastname":"Akarczyk","top":5,"bonus":6}]');
-    expect(actual).toEqual(expected);
+    //const expected = JSON.parse('[{"id":1,"firstname":"Edyta","lastname":"Akarczyk","top":5,"bonus":6}]');
+    // expect(actual).toEqual(expected);
+    expect(actual).toMatchSnapshot();
 });
