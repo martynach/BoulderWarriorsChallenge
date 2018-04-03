6843a31b786fee30513a0840f58d90ddbc54bb54
Martyna <chomicz.martyna@gmail.com>
2018-04-03 02:09:33 +0200

    Some refactor

--- backend/js/Player.js
+++ backend/js/Player.js
@@ -20,59 +20,42 @@ class Player {
 
         await this.loadPlayers();
 
-
-        let expectedPlayers = this.players.map(player => {
-            return { firstname: player.firstname, lastname: player.lastname, id: player.id };
-        }
+        let expectedPlayers = this.players.map(player =>
+            ({ firstname: player.firstname, lastname: player.lastname, id: player.id })
         );
 
-        expectedPlayers = expectedPlayers.sort((player1, player2) => {
-            const lastname1 = player1.lastname.toLowerCase();
-            const lastname2 = player2.lastname.toLowerCase();
+        expectedPlayers.sort((player1, player2) => {
+            const name1 = player1.lastname.toLowerCase() + player1.firstname.toLowerCase();
+            const name2 = player2.lastname.toLowerCase() + player2.firstname.toLowerCase();
 
-            if (lastname1 < lastname2) {
+            if (name1 < name2) {
                 return -1;
             }
-
-            if (lastname1 > lastname2) {
+            if (name1 > name2) {
                 return 1;
             }
-
-            const firstname1 = player1.firstname.toLowerCase();
-            const firstname2 = player2.firstname.toLowerCase();
-
-            if (firstname1 < firstname2) {
-                return -1;
-            }
-
-            if (firstname1 > firstname2) {
-                return 1;
-            }
-
             return 0;
         });
 
         if (gender) {
             return this.filterByGender(expectedPlayers, gender);
         }
-
         return expectedPlayers;
     }
 
     async getPlayersSortedByPoints(gender) {
         await this.loadPlayers();
 
-        let sortedPlayers = this.players.sort((player1, player2) => {
+        this.players.sort((player1, player2) => {
             const point1 = player1.top * 3 + player1.bonus;
             const point2 = player2.top * 3 + player2.bonus;
             return point2 - point1;
         });
 
         if (gender) {
-            return this.filterByGender(sortedPlayers, gender);
+            return this.filterByGender(this.players, gender);
         }
-
-        return sortedPlayers;
+        return this.players;
     }
 
     filterByGender(playersArray, gender) {
@@ -87,17 +70,8 @@ class Player {
             return undefined;
         }
 
-        let filteredPlayers = [];
-        playersArray.forEach(player => {
-            if (player.gender === gender) {
-                filteredPlayers.push(player);
-            }
-        });
-
-        return filteredPlayers;
+        return playersArray.filter(player => player.gender === gender ? true : false);
     }
-
-
 
 }
 
