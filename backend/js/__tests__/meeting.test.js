d0c870e5fec0f219d2fe0aafdf4bdcc69a0164e2
Martyna <chomicz.martyna@gmail.com>
2018-04-04 21:48:42 +0200

    adding more tests for setting result of certain meeting

--- backend/js/__tests__/meeting.test.js
+++ backend/js/__tests__/meeting.test.js
@@ -5,226 +5,219 @@ const player = new Player();
 const path = require("path");
 const meetingsFilepath = path.join(__dirname, './test_data/meetings.json');;
 meeting.filepath = meetingsFilepath;
-player.filepath = path.join(__dirname, './test_data/players.json');
+const playersFilepath = path.join(__dirname, './test_data/players.json');
+player.filepath = playersFilepath;
 meeting.player = player;
 
 const fs = require('fs');
 const promisify = require('./../utils/promisify');
 
 
+test('Get all meetings', async () => {
+    expect.assertions(1);
+    const actual = await meeting.getAllMeetings();
+    expect(actual).toMatchSnapshot();
+});
 
-// test('Get all meetings', async () => {
-//     expect.assertions(1);
-//     const actual = await meeting.getAllMeetings();
-//     expect(actual).toMatchSnapshot();
-// });
+test('Get results for meeting with id=1', async () => {
+    expect.assertions(1);
+    const actual = await meeting.getResults(1);
+    expect(actual).toMatchSnapshot();
+});
 
-// test('Get results for meeting with id=1', async () => {
-//     expect.assertions(1);
-//     const actual = await meeting.getResults(1);
-//     expect(actual).toMatchSnapshot();
-// });
+test('Get results for no existing meeting in the future wth id=2', async () => {
+    expect.assertions(1);
+    const actual = await meeting.getResults(2);
+    expect(actual).toHaveLength(0);
+});
 
-// test('Get results for no existing meeting in the future wth id=2', async () => {
-//     expect.assertions(1);
-//     const actual = await meeting.getResults(2);
-//     expect(actual).toHaveLength(0);
-// });
 
+describe('Tests for adding meeting', () => {
+    const tmpFilepath = path.join(__dirname, './test_data/meetings_tmp.json');
 
-// describe('Tests for adding meeting', () => {
-//     const tmpFilepath = path.join(__dirname, './test_data/meetings_tmp.json');
+    beforeAll(() => {
+        meeting.filepath = tmpFilepath;
+    });
 
-//     beforeAll(() => {
-//         meeting.filepath = tmpFilepath;
-//     });
+    beforeEach(async () => {
+        await promisify(fs.copyFile, meetingsFilepath, tmpFilepath);
 
-//     beforeEach(async () => {
-//         await promisify(fs.copyFile, meetingsFilepath, tmpFilepath);
+    });
 
-//     });
+    afterEach(async () => {
+        await promisify(fs.unlink, tmpFilepath);
+    });
 
-//     afterEach(async () => {
-//         await promisify(fs.unlink, tmpFilepath);
-//     });
+    test('addNewMeeting adding single future meeting', async () => {
+        expect.assertions(2);
 
-//     afterAll(() => {
-//         meeting.filepath = meetingsFilepath;
-//     });
+        const newMeeting = { name: 'new meeting', date: '5.06.2018', numOfBoulders: 10 };
+        await meeting.addNewMeeting(newMeeting);
 
-//     test('addNewMeeting adding single future meeting', async () => {
-//         expect.assertions(2);
+        const meetings = await meeting.getAllMeetings();
+        expect(meetings).toMatchSnapshot();
 
-//         const newMeeting = { name: 'new meeting', date: '5.06.2018', numOfBoulders: 10 };
-//         await meeting.addNewMeeting(newMeeting);
+        const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
+        expect(tmpFileContent).toMatchSnapshot();
+    });
 
-//         const meetings = await meeting.getAllMeetings();
-//         expect(meetings).toMatchSnapshot();
+    test('addNewMeeting adding single meeting with some players', async () => {
+        expect.assertions(2);
 
-//         const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
-//         expect(tmpFileContent).toMatchSnapshot();
-//     });
+        const newMeeting = { name: 'new meeting', date: '5.06.2018', numOfBoulders: 10, players: [1, 2] };
+        await meeting.addNewMeeting(newMeeting);
 
-//     test('addNewMeeting adding single meeting with some players', async () => {
-//         expect.assertions(2);
+        const meetings = await meeting.getAllMeetings();
+        expect(meetings).toMatchSnapshot();
 
-//         const newMeeting = { name: 'new meeting', date: '5.06.2018', numOfBoulders: 10, players: [1, 2] };
-//         await meeting.addNewMeeting(newMeeting);
+        const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
+        expect(tmpFileContent).toMatchSnapshot();
+    });
 
-//         const meetings = await meeting.getAllMeetings();
-//         expect(meetings).toMatchSnapshot();
+    test('addNewMeeting adding single event with incorrect players', async () => {
+        expect.assertions(1);
 
-//         const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
-//         expect(tmpFileContent).toMatchSnapshot();
-//     });
+        const newMeeting = { name: 'new meeting', date: '5.06.2018', numOfBoulders: 10, players: [1, 2, 200] };
+        const expectedError = new Error('Incorrect ids of players: ' + newMeeting.players.toString() + '; ids do not exist in general list of players');
+        await expect(meeting.addNewMeeting(newMeeting)).rejects.toEqual(expectedError);
+    });
 
-//     test('addNewMeeting adding single event with incorrect players', async () => {
-//         expect.assertions(1);
+    test('addNewMeeting adding single event with incorrect properties', async () => {
+        expect.assertions(1);
 
-//         const newMeeting = { name: 'new meeting', date: '5.06.2018', numOfBoulders: 10, players: [1, 2, 200] };
-//         const expectedError = new Error('Incorrect ids of players: ' + newMeeting.players.toString() + '; ids do not exist in general list of players');
-//         await expect(meeting.addNewMeeting(newMeeting)).rejects.toEqual(expectedError);
-//     });
+        const newMeeting = { name: 'new meeting', date: '5.06.2018', numOfBoulders: 10, results: 'some result' };
 
-//     test('addNewMeeting adding single event with incorrect properties', async () => {
-//         expect.assertions(1);
+        const expectedError = new Error('Incorrect properties of new meeting playload (name, date, numOfBoulders, players)');
+        await expect(meeting.addNewMeeting(newMeeting)).rejects.toEqual(expectedError);
+    });
+});
 
-//         const newMeeting = { name: 'new meeting', date: '5.06.2018', numOfBoulders: 10, results: 'some result' };
 
-//         const expectedError = new Error('Incorrect properties of new meeting playload (name, date, numOfBoulders, players)');
-//         await expect(meeting.addNewMeeting(newMeeting)).rejects.toEqual(expectedError);
-//     });
+describe('Tests for adding boulders to meeting with given id', () => {
+    const tmpFilepath = path.join(__dirname, './test_data/meetings_tmp.json');
 
+    beforeAll(() => {
+        meeting.filepath = tmpFilepath;
+    });
 
+    beforeEach(async () => {
+        await promisify(fs.copyFile, meetingsFilepath, tmpFilepath);
 
-// });
+    });
 
+    afterEach(async () => {
+        await promisify(fs.unlink, tmpFilepath);
+        meeting.meetings = null;
+    });
 
-// describe('Tests for adding boulders to meeting with given id', () => {
-//     const tmpFilepath = path.join(__dirname, './test_data/meetings_tmp.json');
 
-//     beforeAll(() => {
-//         meeting.filepath = tmpFilepath;
-//     });
+    test('addNewBoulders proper number and meetingId', async () => {
+        expect.assertions(2);
 
-//     beforeEach(async () => {
-//         await promisify(fs.copyFile, meetingsFilepath, tmpFilepath);
+        const newBouldersPayload = { meetingId: 2, numOfBoulders: 5 };
+        await meeting.addNewBoulders(newBouldersPayload);
 
-//     });
+        const numOfBoulders = await meeting.getNumberOfBoulders(newBouldersPayload.meetingId);
+        expect(numOfBoulders).toBe(45);
 
-//     afterEach(async () => {
-//         await promisify(fs.unlink, tmpFilepath);
-//         meeting.meetings = null;
-//     });
+        const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
+        expect(tmpFileContent).toMatchSnapshot();
+    });
 
-//     afterAll(() => {
-//         meeting.filepath = filepath;
-//     });
+    test('addNewBoulders -removing boulders- proper number and meetingId', async () => {
+        expect.assertions(2);
 
-//     test('addNewBoulders proper number and meetingId', async () => {
-//         expect.assertions(2);
+        const newBouldersPayload = { meetingId: 2, numOfBoulders: -5 };
+        await meeting.addNewBoulders(newBouldersPayload);
 
-//         const newBouldersPayload = { meetingId: 2, numOfBoulders: 5 };
-//         await meeting.addNewBoulders(newBouldersPayload);
+        const numOfBoulders = await meeting.getNumberOfBoulders(newBouldersPayload.meetingId);
+        expect(numOfBoulders).toBe(35);
 
-//         const numOfBoulders = await meeting.getNumberOfBoulders(newBouldersPayload.meetingId);
-//         expect(numOfBoulders).toBe(45);
+        const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
+        expect(tmpFileContent).toMatchSnapshot();
+    });
 
-//         const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
-//         expect(tmpFileContent).toMatchSnapshot();
-//     });
+    test('addNewBoulders bad meetingId', async () => {
+        expect.assertions(1);
 
-//     test('addNewBoulders -removing boulders- proper number and meetingId', async () => {
-//         expect.assertions(2);
+        const newBouldersPayload = { meetingId: 5, numOfBoulders: 8 }
+        const expectedError = new Error(`Not existing meetingId: ${newBouldersPayload.meetingId}`);
+        await expect(meeting.addNewBoulders(newBouldersPayload)).rejects.toEqual(expectedError);
+    });
 
-//         const newBouldersPayload = { meetingId: 2, numOfBoulders: -5 };
-//         await meeting.addNewBoulders(newBouldersPayload);
+    test('addNewBoulders inproper payload properties', async () => {
+        expect.assertions(1);
 
-//         const numOfBoulders = await meeting.getNumberOfBoulders(newBouldersPayload.meetingId);
-//         expect(numOfBoulders).toBe(35);
+        const newBouldersPayload = { meetingId: 2, numOfBoulders: 7, unexpected: 0 }
+        const expectedError = new Error('Incorrect properties of new boulder playload (meetingId, numOfBoulders)');
+        await expect(meeting.addNewBoulders(newBouldersPayload)).rejects.toEqual(expectedError);
+    });
 
-//         const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
-//         expect(tmpFileContent).toMatchSnapshot();
-//     });
+});
 
-//     test('addNewBoulders bad meetingId', async () => {
-//         expect.assertions(1);
 
-//         const newBouldersPayload = { meetingId: 5, numOfBoulders: 8 }
-//         const expectedError = new Error(`Not existing meetingId: ${newBouldersPayload.meetingId}`);
-//         await expect(meeting.addNewBoulders(newBouldersPayload)).rejects.toEqual(expectedError);
-//     });
 
-//     test('addNewBoulders inproper payload properties', async () => {
-//         expect.assertions(1);
+describe('Tests for adding players to meeting with given id', () => {
+    const tmpFilepath = path.join(__dirname, './test_data/meetings_tmp.json');
+    const tmpPlayerFilepath = path.join(__dirname, './test_data/players_tmp.json');
 
-//         const newBouldersPayload = { meetingId: 2, numOfBoulders: 7, unexpected: 0 }
-//         const expectedError = new Error('Incorrect properties of new boulder playload (meetingId, numOfBoulders)');
-//         await expect(meeting.addNewBoulders(newBouldersPayload)).rejects.toEqual(expectedError);
-//     });
 
-// });
+    beforeAll(() => {
+        meeting.filepath = tmpFilepath;
+        player.filepath = tmpPlayerFilepath;
+    });
 
+    beforeEach(async () => {
+        await promisify(fs.copyFile, meetingsFilepath, tmpFilepath);
+        await promisify(fs.copyFile, playersFilepath, tmpPlayerFilepath);
 
+    });
 
-// describe('Tests for adding players to meeting with given id', () => {
-//     const tmpFilepath = path.join(__dirname, './test_data/meetings_tmp.json');
+    afterEach(async () => {
+        await promisify(fs.unlink, tmpFilepath);
+        await promisify(fs.unlink, tmpPlayerFilepath);
+        meeting.meetings = null;
+    });
 
-//     beforeAll(() => {
-//         meeting.filepath = tmpFilepath;
-//     });
 
-//     beforeEach(async () => {
-//         await promisify(fs.copyFile, meetingsFilepath, tmpFilepath);
+    test('addNewPlayers proper playersIds and meetingId', async () => {
+        expect.assertions(2);
 
-//     });
+        const newPlayersPayload = { meetingId: 2, players: [2, 3, 4] }
+        await meeting.addNewPlayers(newPlayersPayload);
 
-//     afterEach(async () => {
-//         await promisify(fs.unlink, tmpFilepath);
-//     });
+        const players = await meeting.getPlayersIds(newPlayersPayload.meetingId);
+        expect(players).toEqual(expect.arrayContaining([1, 2, 3, 4, 9]));
 
-//     afterAll(() => {
-//         meeting.filepath = filepath;
-//     });
+        const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
+        expect(tmpFileContent).toMatchSnapshot();
+    });
 
-//     test('addNewPlayers proper playersIds and meetingId', async () => {
-//         expect.assertions(2);
+    test('addNewPlayers bad meetingId', async () => {
+        expect.assertions(1);
 
-//         const newPlayersPayload = { meetingId: 2, players: [2, 3, 4] }
-//         await meeting.addNewPlayers(newPlayersPayload);
+        const newPlayersPayload = { meetingId: 4, players: [2, 3, 4] }
+        const expectedError = new Error(`Not existing meetingId: ${newPlayersPayload.meetingId}`)
+        await expect(meeting.addNewPlayers(newPlayersPayload)).rejects.toEqual(expectedError);
+    });
 
-//         const players = await meeting.getPlayersIds(newPlayersPayload.meetingId);
-//         expect(players).toEqual(expect.arrayContaining([1, 2, 3, 4, 9]));
+    test('addNewPlayers bad playerIds - they are already in the meeting', async () => {
+        expect.assertions(1);
 
-//         const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
-//         expect(tmpFileContent).toMatchSnapshot();
-//     });
+        const newPlayersPayload = { meetingId: 2, players: [1, 2] } //player 1 already exists
+        const expectedError = new Error('Incorrect ids of players: ' + newPlayersPayload.players.toString() + '; ids already exist in this meeting');
+        await expect(meeting.addNewPlayers(newPlayersPayload)).rejects.toEqual(expectedError);
+    });
 
-//     test('addNewPlayers bad meetingId', async () => {
-//         expect.assertions(1);
+    test('addNewPlayers bad playerIds - the are not in general list of players', async () => {
+        expect.assertions(1);
 
-//         const newPlayersPayload = { meetingId: 4, players: [2, 3, 4] }
-//         const expectedError = new Error(`Not existing meetingId: ${newPlayersPayload.meetingId}`)
-//         await expect(meeting.addNewPlayers(newPlayersPayload)).rejects.toEqual(expectedError);
-//     });
+        const newPlayersPayload = { meetingId: 2, players: [2, 30] } //player with id 30 does not exist
+        const expectedError = new Error('Incorrect ids of players: ' + newPlayersPayload.players.toString() + '; ids do not exist in general list of players');
+        await expect(meeting.addNewPlayers(newPlayersPayload)).rejects.toEqual(expectedError);
+    });
 
-//     test('addNewPlayers bad playerIds - they are already in the meeting', async () => {
-//         expect.assertions(1);
-
-//         const newPlayersPayload = { meetingId: 2, players: [1, 2] } //player 1 already exists
-//         const expectedError = new Error('Incorrect ids of players: ' + newPlayersPayload.players.toString() + '; ids already exist in this meeting');
-//         await expect(meeting.addNewPlayers(newPlayersPayload)).rejects.toEqual(expectedError);
-//     });
-
-//     test('addNewPlayers bad playerIds - the are not in general list of players', async () => {
-//         expect.assertions(1);
-
-//         const newPlayersPayload = { meetingId: 2, players: [2, 30] } //player with id 30 does not exist
-//         const expectedError = new Error('Incorrect ids of players: ' + newPlayersPayload.players.toString() + '; ids do not exist in general list of players');
-//         await expect(meeting.addNewPlayers(newPlayersPayload)).rejects.toEqual(expectedError);
-//     });
-
-// });
+});
 
 describe('Tests for setting results of meeting with given id', () => {
     const tmpFilepath = path.join(__dirname, './test_data/meetings_tmp.json');
@@ -247,7 +240,7 @@ describe('Tests for setting results of meeting with given id', () => {
     });
 
     test('setResultsOfMeeting proper values', async () => {
-        expect.assertions(2);
+        expect.assertions(1);
 
         const newResultsPayload = {
             meetingId: 2, results: [
@@ -260,4 +253,36 @@ describe('Tests for setting results of meeting with given id', () => {
         const tmpFileContent = await promisify(fs.readFile, tmpFilepath, 'utf8');
         expect(tmpFileContent).toMatchSnapshot();
     });
+
+    test('setResultsOfMeeting incorrect properties', async () => {
+        expect.assertions(1);
+
+        const newResultsPayload = {
+            meetingId: 2, resultsEE: [
+                { playerID: 1, top: 30, bonus: 40 },
+                { playerID: 9, top: 34, bonus: 39 },
+            ]
+        };
+
+        const expectedError = new Error('Incorrect properties of new results playload - object (meetingId, results array {playerID, top, bonus}) is required');
+        await expect(meeting.setResultsOfMeeting(newResultsPayload)).rejects.toEqual(expectedError);
+
+    });
+
+    test('setResultsOfMeeting incorrect properties - no playerID', async () => {
+        expect.assertions(1);
+
+        const newResultsPayload = {
+            meetingId: 2, results: [
+                {  top: 30, bonus: 40 },
+                { playerID: 9, top: 34, bonus: 39 },
+            ]
+        };
+
+        const expectedError = new Error('Incorrect properties of new results playload - object (meetingId, results array {playerID, top, bonus}) is required');
+        await expect(meeting.setResultsOfMeeting(newResultsPayload)).rejects.toEqual(expectedError);
+
+    });
+
+
 });
